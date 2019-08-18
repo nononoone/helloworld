f8aa1964fc947cca98e8a52b14fe2918a0be692f
nononoone <nononoone@fake.com>
2019-08-19 06:54:49 +0800

    add some plugin, create docs directory

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(App) },
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const path = require('path')
+
 module.exports = {
+  configureWebpack: {
+    entry: {
+      app: './src/renderer/main.js'
+    },
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/renderer')
+      },
+      extensions: ['.js', '.vue', '.json', '.css', '.node']
+    },
+    target: 'electron-renderer'
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
     : './'

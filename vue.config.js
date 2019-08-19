a1a3b5376a208918f1dc908fac9316ea03b75965
nononoone <nononoone@fake.com>
2019-08-19 23:38:55 +0800

    恢复vue目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
-const path = require('path')
-
 module.exports = {
-  configureWebpack: {
-    entry: {
-      app: './src/renderer/main.js'
-    },
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src/renderer')
-      },
-      extensions: ['.js', '.vue', '.json', '.css', '.node']
-    },
-    target: 'electron-renderer'
-  },
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
     : './'
